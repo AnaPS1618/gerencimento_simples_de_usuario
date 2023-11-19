1a226cac92d14509f57ee46a27f89903beec0142
Ana <anasouza673@gmail.com>
2023-11-19 13:01:01 -0300

    index

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const { rotas } = require('./routes/rotas')
+
+const app = express()
+
+
+app.use(express.json())
+app.use(rotas)
+
+app.listen(8000, () => {
+    console.log('rodando na porta 8000')
+})
