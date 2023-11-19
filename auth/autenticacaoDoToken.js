a89758e8c051116574ef95d9db1f89958272882f
Ana <anasouza673@gmail.com>
2023-11-19 13:03:33 -0300

    autenticação

---
+++ auth/autenticacaoDoToken.js
@@ -0,0 +1,45 @@
+const jwt = require('jsonwebtoken');
+const senhaSecreta = require('../auth/senhaJWT');
+const pool = require('../config/conexao');
+
+
+const validandoToken = async (req, res, next) => {
+
+    const {authorization} = req.headers;
+
+    if(!authorization){
+        return res.status(401)
+        .json('Para acessar este recurso um token de autenticação válido deve ser enviado')
+    }
+
+    try{
+        const token = authorization.split(" ")[1];
+
+        const {id} = jwt.verify(token, senhaSecreta)
+
+        const buscandoUsuario = `
+            select * from cadastro 
+            where id = $1;
+        `
+
+        const {rows, rowCount} = await pool.query(buscandoUsuario,
+                [id]
+        )
+
+        if(rowCount < 1){
+            return res.status(401)
+                        .json('Para acessar este recurso um token de autenticação válido deve ser enviado.')
+        }
+
+        req.usuario = rows[0]
+
+        next();
+    }catch(error){
+        console.log(error.message)
+        return res.status(500)
+        .json('Para acessar este recurso um token de autenticação válido deve ser enviado.')
+    }
+
+}
+
+module.exports = validandoToken;
---
+++ auth/senhaJWT.js
@@ -0,0 +1,3 @@
+const senhaSecreta = 'minhaSenhaSecreta';
+
+module.exports = senhaSecreta;
